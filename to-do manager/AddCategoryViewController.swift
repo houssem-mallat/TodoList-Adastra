1f88142edc15b7c5c4639e64cda8aee5fdf02a0c
Houssem Mallat <houssemmallat@MacBook-Pro.local>
2019-06-04 17:48:14 +0100

    Initial Commit

---
+++ to-do manager/AddCategoryViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddCategoryViewController.swift
+//  to-do manager
+//
+//  Created by Houssem Mallat on 6/3/19.
+//  Copyright © 2019 Houssem Mallat. All rights reserved.
+//
+
+import UIKit
+
+class AddCategoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ to-do manager/ToDo.swift
@@ -0,0 +1,9 @@
+//
+//  ToDo.swift
+//  to-do manager
+//
+//  Created by Houssem Mallat on 6/3/19.
+//  Copyright © 2019 Houssem Mallat. All rights reserved.
+//
+
+import Foundation
